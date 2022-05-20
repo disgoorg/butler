b166c3da90f1cf2b4ce8085116b2a3361e656666
TopiSenpai <tobi@strasserhof.com>
2022-05-20 13:55:04 +0200

    implement info & ping command

--- commands/info.go
+++ commands/info.go
@@ -1,7 +1,9 @@
 package commands
 
 import (
+	"github.com/disgoorg/disgo"
 	"github.com/disgoorg/disgo-butler/butler"
+	"github.com/disgoorg/disgo-butler/common"
 	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/events"
 )
@@ -17,11 +19,17 @@ var InfoCommand = butler.Command{
 }
 
 func handleInfo(b *butler.Butler, e *events.ApplicationCommandInteractionEvent) error {
+	user, _ := b.Client.Caches().GetSelfUser()
 	return e.CreateMessage(discord.NewMessageCreateBuilder().
 		SetEmbeds(discord.NewEmbedBuilder().
-			AddField("Version", "", false).
+			SetTitle("DisGo Butler").
+			SetThumbnail(user.EffectiveAvatarURL()).
+			SetColor(common.ColorSuccess).
+			AddField("Version", b.Version, false).
+			AddField("DisGo Version", disgo.Version, false).
 			Build(),
 		).
+		AddActionRow(discord.NewLinkButton("GitHub", "https://github.com/disgoorg/disgo-butler")).
 		Build(),
 	)
 }
--- commands/ping.go
+++ commands/ping.go
@@ -1,9 +1,13 @@
 package commands
 
 import (
+	"time"
+
 	"github.com/disgoorg/disgo-butler/butler"
+	"github.com/disgoorg/disgo-butler/common"
 	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/rest"
 )
 
 var PingCommand = butler.Command{
@@ -17,5 +21,31 @@ var PingCommand = butler.Command{
 }
 
 func handlePing(_ *butler.Butler, e *events.ApplicationCommandInteractionEvent) error {
-	return e.CreateMessage(discord.MessageCreate{Content: "Pong"})
+	var gatewayPing string
+	if e.Client().HasGateway() {
+		gatewayPing = e.Client().Gateway().Latency().String()
+	}
+
+	eb := discord.NewEmbedBuilder().
+		SetTitle("Ping").
+		AddField("Rest", "loading...", false).
+		AddField("Gateway", gatewayPing, false).
+		SetColor(common.ColorSuccess)
+
+	defer func() {
+		var start int64
+		_, _ = e.Client().Rest().GetBotApplicationInfo(func(config *rest.RequestConfig) {
+			start = time.Now().UnixNano()
+		})
+		duration := time.Now().UnixNano() - start
+		eb.SetField(0, "Rest", time.Duration(duration).String(), false)
+		if _, err := e.Client().Rest().UpdateInteractionResponse(e.ApplicationID(), e.Token(), discord.MessageUpdate{Embeds: &[]discord.Embed{eb.Build()}}); err != nil {
+			e.Client().Logger().Error("Failed to update ping embed: ", err)
+		}
+	}()
+
+	return e.Respond(discord.InteractionCallbackTypeCreateMessage, discord.NewMessageCreateBuilder().
+		SetEmbeds(eb.Build()).
+		Build(),
+	)
 }
